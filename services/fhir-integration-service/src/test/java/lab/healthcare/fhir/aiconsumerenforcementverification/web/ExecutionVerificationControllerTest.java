6337d97b75961cbf176000c7925a4d312576e7dd
Abel Vergaray <avergarayb@gmail.com>
2026-09-12 19:11:18 -0500

    fix: shorten task 071 test filenames for windows path limits

--- services/fhir-integration-service/src/test/java/lab/healthcare/fhir/aiconsumerenforcementverification/web/ExecutionVerificationControllerTest.java
+++ services/fhir-integration-service/src/test/java/lab/healthcare/fhir/aiconsumerenforcementverification/web/ExecutionVerificationControllerTest.java
@@ -29,7 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AiConsumerClinicalDataEnforcementExecutionVerificationController.class)
-class AiConsumerClinicalDataEnforcementExecutionVerificationControllerTest {
+class ExecutionVerificationControllerTest {
 
     private static final String HIDDEN_PATIENT = "lab-hidden-patient";
 
